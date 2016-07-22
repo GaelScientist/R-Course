606ebc8a9a8ea4d3d863bbcad281694f2c5dd310
GaelScientist <gael.peron@hotmail.fr>
2016-07-22 19:28:00 +0200

    Add files via upload

--- apply-family.r
+++ apply-family.r
@@ -11,3 +11,37 @@ lapply(temp, max)
 
 # Use sapply() to find each day's maximum temperature
 sapply(temp, max)
+
+# temp is already available in the workspace
+
+# Definition of print_info()
+print_info <- function(x) {
+  cat("The average temperature is", mean(x), "\n")
+}
+
+# Apply print_info() over temp using lapply()
+lapply(temp, print_info)
+
+# Apply print_info() over temp using sapply()
+sapply(temp, print_info)
+#sapply can't simplify functions that return NULL
+
+# temp is already prepared for you in the workspace
+
+# Definition of below_zero()
+below_zero <- function(x) {
+  return(x[x < 0])
+}
+
+# Apply below_zero over temp using sapply(): freezing_s
+freezing_s <- sapply(temp, below_zero)
+
+# Apply below_zero over temp using lapply(): freezing_l
+freezing_l <- lapply(temp, below_zero)
+
+# Are freezing_s and freezing_l identical?
+identical(freezing_l, freezing_s)
+
+#sapply is not able to nicely convert the output of lapply() to a nicely formatted matrix
+
+sapply(list(runif(10), runif(10)), function(x) c(min = min(x), mean = mean(x), max = max(x)))
