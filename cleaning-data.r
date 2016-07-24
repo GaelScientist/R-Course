d1b6a98c9cd3b4ec7ec76523767a9d6a20f0a826
GaelScientist <gael.peron@hotmail.fr>
2016-07-24 20:32:09 +0200

    Update cleaning-data.r

--- cleaning-data.r
+++ cleaning-data.r
@@ -41,7 +41,7 @@ tail(bmi)
 tail(bmi, n = 10)
 
 # Histogram of BMIs from 2008
-hist(bmi$2008)
+hist(bmi$Y2008)
 
 # Scatter plot comparing BMIs from 1980 to those from 2008
-plot(bmi$1980, bmi$2008)
+plot(bmi$Y1980, bmi$Y2008)
