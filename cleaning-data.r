4cb3b402e343b28ec35c5006531545de64fe48f1
GaelScientist <gael.peron@hotmail.fr>
2016-07-25 23:36:07 +0200

    Update cleaning-data.r

--- cleaning-data.r
+++ cleaning-data.r
@@ -57,3 +57,28 @@ bmi_wide <- spread(bmi_long, year, bmi_val)
 
 # View the head of bmi_wide
 head(bmi_wide)
+
+bmi_cc_clean <- separate(bmi_cc, col = Country_ISO, into = c("Country", "ISO"), sep = "/")
+
+# Print the head of the result
+head(bmi_cc_clean)
+
+## tidyr is already loaded for you
+
+# View first 50 rows of census_long
+head(census_long, n = 50)
+
+# Spread the type column
+census_long2 <- spread(census_long, type, amount)
+
+# View first 20 rows of census_long2
+head(census_long2, n = 20)
+
+# View the head of census_long3
+head(census_long3)
+
+# Separate the yr_month column into two
+census_long4 <- separate(census_long3, yr_month, c("year", "month"))
+
+# View the first 6 rows of the result
+head(census_long4)
