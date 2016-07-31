e684102eb2f8991aa87eb1537c9d6bee43643cd3
GaelScientist <gael.peron@hotmail.fr>
2016-07-31 10:08:17 +0200

    Update cleaning-data.r

--- cleaning-data.r
+++ cleaning-data.r
@@ -82,3 +82,51 @@ census_long4 <- separate(census_long3, yr_month, c("year", "month"))
 
 # View the first 6 rows of the result
 head(census_long4)
+
+# Make this evaluate to character
+class(as.character(TRUE))
+
+# Make this evaluate to numeric
+class(as.numeric("8484.00"))
+
+# Make this evaluate to integer
+class(as.integer(99L))
+
+# Make this evaluate to factor
+class(as.factor("factor"))
+
+# Make this evaluate to logical
+class(as.logical("FALSE"))
+
+# Preview students with str()
+str(students)
+
+# Coerce Grades to character
+students$Grades <-as.character(students$Grades)
+
+# Coerce Medu to factor
+students$Medu <- as.factor(students$Medu)
+
+# Coerce Fedu to factor
+students$Fedu <- as.factor(students$Fedu)
+    
+# Look at students once more with str()
+str(students)
+
+# Load the lubridate package
+require("lubridate")
+
+# Parse as date
+dmy("17 Sep 2015")
+
+# Parse as date and time (with no seconds!)
+mdy_hm("July 15, 2012 12:56")
+
+# Coerce dob to a date (with no time)
+students2$dob <- ymd(students2$dob)
+
+# Coerce nurse_visit to a date and time
+students2$nurse_visit <- ymd_hms(students2$nurse_visit)
+    
+# Look at students2 once more with str()
+str(students2)
