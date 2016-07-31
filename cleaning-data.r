2ac96e2da7d82149269776726c42c4b52765cc5d
GaelScientist <gael.peron@hotmail.fr>
2016-07-31 10:29:40 +0200

    Update cleaning-data.r

--- cleaning-data.r
+++ cleaning-data.r
@@ -130,3 +130,12 @@ students2$nurse_visit <- ymd_hms(students2$nurse_visit)
     
 # Look at students2 once more with str()
 str(students2)
+
+# Load the stringr package
+require("stringr")
+
+# Trim all leading and trailing whitespace
+str_trim(c("   Filip ", "Nick  ", " Jonathan"))
+
+# Pad these strings with leading zeros
+str_pad(c("23485W", "8823453Q", "994Z") , width = 9, side = "left", pad = "0")
